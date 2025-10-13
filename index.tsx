b2d2e9eca04af0729baafedbe8506d556f38537e
anonymousbeefsteak-cloud <anonymousbeefsteak@gmail.com>
2025-10-13 20:54:01 +0800

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,16 +1,2 @@
-
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// This file is part of a previous version and is no longer used.
+// The main application entry point is located at /src/main.tsx.
